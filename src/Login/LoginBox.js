ed15b07975d5cf325204cfebba50b4b6b8c850fc
Ayon <ayonmahmud53@gmail.com>
2020-06-18 14:15:41 +0600

    Made responsive after adding "form tag"

--- src/Login/LoginBox.js
+++ src/Login/LoginBox.js
@@ -60,91 +60,89 @@ export default class LoginBox extends Component {
               <p className="active text-center font-weight-bold login-text">
                 Log In
               </p>
-              <br />
-              <form onSubmit={this.sendForm}>
-                <div className="form-group">
-                  <div className="input-field">
-                    <input
-                      type="email"
-                      className="form-control rounded-pill   form-input-background "
-                      name="email"
-                      value={this.state.email}
-                      onInput={this.handleInput}
-                      id="name"
-                      onChange={(e) =>
-                        this.setState({
-                          email: e.target.value,
-                        })
-                      }
-                      required
-                    />
-                    <label htmlFor="name">Email</label>
-                  </div>
 
-                  <div className="input-field ">
-                    <input
-                      type="password"
-                      className="form-control rounded-pill   form-input-background "
-                      name="password"
-                      value={this.state.password}
-                      onInput={this.handleInput}
-                      id="password"
-                      onChange={(e) =>
-                        this.setState({
-                          password: e.target.value,
-                        })
-                      }
-                      required
-                    />
-                    <label htmlFor="password">Password</label>
-                  </div>
-
-                  <p className="Forgot_pass">
-                    <a href="/forgetPassword" style={{ color: "#2D2E6A" }}>
-                      Forgot Password
-                    </a>
-                  </p>
-
-                  <button
-                    type="submit"
-                    className="btn btn-block text-white text-center"
-                    style={{
-                      marginTop: "10px",
-                      marginBottom: "20px",
-                      marginLeft: "auto",
-                      marginRight: "auto",
-                      width: "150px",
-                      borderRadius: "1em",
-                      height: "35px",
-                      backgroundColor: "#0C0C52",
-                      fontSize: "14px",
-                    }}
-                  >
-                    LOG IN
-                  </button>
-
-                  <p className="h6  text-center" style={{ color: "#292A67" }}>
-                    Don't have an account?
-                  </p>
-
-                  <a
-                    href="/register"
-                    className="btn btn-block text-white text-center"
-                    style={{
-                      marginTop: "20px",
-                      marginBottom: "10px",
-                      marginLeft: "auto",
-                      marginRight: "auto",
-                      width: "150px",
-                      borderRadius: "1em",
-                      height: "35px",
-                      backgroundColor: "#0C0C52",
-                      fontSize: "14px",
-                    }}
-                  >
-                    SIGN UP
-                  </a>
+              <form className="form-group" onSubmit={this.sendForm}>
+                <div className="input-field">
+                  <input
+                    type="email"
+                    className="form-control rounded-pill   form-input-background "
+                    name="email"
+                    value={this.state.email}
+                    onInput={this.handleInput}
+                    id="name"
+                    onChange={(e) =>
+                      this.setState({
+                        email: e.target.value,
+                      })
+                    }
+                    required
+                  />
+                  <label htmlFor="name">Email</label>
                 </div>
+
+                <div className="input-field ">
+                  <input
+                    type="password"
+                    className="form-control rounded-pill   form-input-background "
+                    name="password"
+                    value={this.state.password}
+                    onInput={this.handleInput}
+                    id="password"
+                    onChange={(e) =>
+                      this.setState({
+                        password: e.target.value,
+                      })
+                    }
+                    required
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+
+                <p className="Forgot_pass">
+                  <a href="/forgetPassword" style={{ color: "#2D2E6A" }}>
+                    Forgot Password
+                  </a>
+                </p>
+
+                <button
+                  type="submit"
+                  className="btn btn-block text-white text-center"
+                  style={{
+                    marginTop: "10px",
+                    marginBottom: "20px",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    width: "150px",
+                    borderRadius: "1em",
+                    height: "35px",
+                    backgroundColor: "#0C0C52",
+                    fontSize: "14px",
+                  }}
+                >
+                  LOG IN
+                </button>
+
+                <p className="h6  text-center" style={{ color: "#292A67" }}>
+                  Don't have an account?
+                </p>
+
+                <a
+                  href="/register"
+                  className="btn btn-block text-white text-center"
+                  style={{
+                    marginTop: "20px",
+                    marginBottom: "10px",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    width: "150px",
+                    borderRadius: "1em",
+                    height: "35px",
+                    backgroundColor: "#0C0C52",
+                    fontSize: "14px",
+                  }}
+                >
+                  SIGN UP
+                </a>
               </form>
             </MDBCol>
           </div>
